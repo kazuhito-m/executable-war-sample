bcccda1f9aa6997d5cff2bc1d0b2624d23bd1d27
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-11-26 21:45:44 +0900

    実装。パラメータを解析するクラスと、そのテストを追加。

---
+++ src/main/java/com/github/kazuhito_m/Parameters.java
@@ -0,0 +1,37 @@
+package com.github.kazuhito_m;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+class Parameters {
+
+    String contextRoot() {
+        return findParameterValue("-c", "--ContextRoot")
+                .map(c -> c.trim().replaceAll("^\\/*", ""))
+                .map(c -> c.isEmpty() ? "" : "/" + c)
+                .orElse("");
+    }
+
+    int port() {
+        return findParameterValue("-p", "--Port")
+                .map(Integer::valueOf)
+                .orElse(8080);
+    }
+
+    private Optional<String> findParameterValue(String... option) {
+        List<String> optionPatterns = Arrays.asList(option);
+        for (int i = 0; i < consoleArguments.length - 1; i++) {
+            String parameter = consoleArguments[i];
+            if (optionPatterns.contains(parameter)) return Optional.of(consoleArguments[i + 1]);
+        }
+        return Optional.empty();
+    }
+
+    final String[] consoleArguments;
+
+    Parameters(String[] consoleArguments) {
+        this.consoleArguments = consoleArguments;
+    }
+
+}
---
+++ src/test/java/com/github/kazuhito_m/ParametersTest.java
@@ -0,0 +1,50 @@
+package com.github.kazuhito_m;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParametersTest {
+
+    @Test
+    @DisplayName("何もパラメータが指定されていなければデフォルトを返す。")
+    public void nonParameterTest() {
+        Parameters sut = new Parameters(new String[]{});
+        assertEquals(sut.contextRoot(), "", "コンテキストパスの初期値は空文字。");
+        assertEquals(sut.port(), 8080, "ポートの初期値は8080。");
+    }
+
+    @Test
+    @DisplayName("コンテキストパスの指定で/がなければ付け加える。")
+    public void contextPathAddSlash() {
+        Parameters sut = new Parameters(new String[]{"--ContextRoot", "testContextPath"});
+        assertEquals(sut.contextRoot(), "/testContextPath", "スラッシュがつけくわわってる。");
+    }
+
+    @Test
+    @DisplayName("コンテキストパスの指定で、先頭に/が連打されてたら削除する。")
+    public void contextPathRemoveOnlyOneSlash() {
+        Parameters sut = new Parameters(new String[]{"-c", "/////testContextPath"});
+        assertEquals(sut.contextRoot(), "/testContextPath", "スラッシュが削除されてる。");
+    }
+
+    @Test
+    @DisplayName("ポートの指定で、数値を指定すればそのまま数値として出る。")
+    public void portStraight() {
+        Parameters sut = new Parameters(new String[]{"-p", "1234"});
+        assertEquals(sut.port(), 1234, "ポートが取れる。");
+    }
+
+    @Test
+    @DisplayName("ポートの指定で、数値を以外を指定すれば例外となる。")
+    public void portNonNumeric() {
+        Parameters sut = new Parameters(new String[]{"--Port", "STRING"});
+        assertThrows(NumberFormatException.class, () -> {
+            sut.port();
+        });
+    }
+
+}
